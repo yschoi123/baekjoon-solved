371cc68ff225bde26a71081096bcd493e6209ae5
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2024-04-30 11:26:40 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 196 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.java
@@ -1,22 +1,26 @@
 
 import java.util.*;
 public class Main {
+    public static int[] countString(String str) {
+        int[] count = new int[26];
+        for(int i=0; i<str.length(); i++) {
+            count[str.charAt(i)-'a']++;
+        }
+        return count;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str1 = sc.next();
         String str2 = sc.next();
 
-        int sameCnt = 0;
+        int[] str1Count = countString(str1);
+        int[] str2Count = countString(str2);
 
-        for(int i=0; i<str1.length(); i++) {
-            int idx = str2.indexOf(str1.charAt(i));
-            if(idx != -1) {
-                sameCnt++;
-                str2 = str2.substring(0, idx) + "_" + str2.substring(idx+1);
-            }
+        int ans = 0;
+
+        for(int i=0; i<str1Count.length; i++) {
+            ans += Math.abs(str1Count[i]-str2Count[i]);
         }
-
-        int ans = str1.length() + str2.length() - sameCnt * 2;
         System.out.print(ans);
     }
 }
