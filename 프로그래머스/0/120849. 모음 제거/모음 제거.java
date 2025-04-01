9ac8b2c4739b2415f4e95f530a51feedb967c5cd
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2025-04-01 23:57:55 +0900

    [level 0] Title: 모음 제거, Time: 0.15 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,11 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        String[] targets = {"a", "e", "i", "o", "u"};
-        
-        for(String target : targets) {
-            my_string = my_string.replace(target, "");
-        }
 
-        return my_string;
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
