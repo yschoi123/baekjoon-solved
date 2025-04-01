edc4ca58f579fd50d47a15ad0e25d486e54aedd2
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2025-04-02 03:44:52 +0900

    [level 0] Title: 평행, Time: 0.05 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/README.md
+++ 프로그래머스/0/120875. 평행/README.md
@@ -1,10 +1,10 @@
 # [level 0] 평행 - 120875 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875) 
 
 ### 성능 요약
 
-메모리: 72.7 MB, 시간: 0.03 ms
+메모리: 81.6 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 02일 03:26:20
+2025년 04월 02일 03:44:50
 
 ### 문제 설명
 
--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,32 +1,25 @@
 import java.util.*;
 
 class Solution {
+    public double slope(int x1, int y1, int x2, int y2) {
+      return  (double)(y2 - y1) / (x2 - x1);
+    }
+    
+    public boolean isEqualSlope(int[][] dots, int line1dot1, int line1dot2, int line2dot1, int lien2dot2) {
+        double dydx1 = slope(dots[line1dot1][0], dots[line1dot1][1], dots[line1dot2][0], dots[line1dot2][1]);
+        double dydx2 = slope(dots[line2dot1][0], dots[line2dot1][1], dots[lien2dot2][0], dots[lien2dot2][1]);
+        
+        return dydx1 == dydx2;
+    }
+        
     public int solution(int[][] dots) {
         List<Double> dydxList = new ArrayList<>();
         
-        double dydx1 = (double)(dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0]);
-        double dydx2 = (double)(dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]);
-        if(dydx1 == dydx2) {
-            return 1;
-        }
-        
-        dydx1 = (double)(dots[2][1] - dots[0][1]) / (dots[2][0] - dots[0][0]);
-        dydx2 = (double)(dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0]);
-        if(dydx1 == dydx2) {
-            return 1;
-        }
-        
-        dydx1 = (double)(dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0]);
-        dydx2 = (double)(dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0]);
-        if(dydx1 == dydx2) {
-            return 1;
-        }
-        
-        dydx1 = (double)(dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0]);
-        dydx2 = (double)(dots[2][1] - dots[1][1]) / (dots[2][0] - dots[1][0]);
-        if(dydx1 == dydx2) {
+        if(isEqualSlope(dots, 0, 1, 2, 3) || isEqualSlope(dots, 0, 2, 1, 3) || isEqualSlope(dots, 0, 3, 1, 2)) {
             return 1;
         }
         return 0;
     }
+    
+    
 }
