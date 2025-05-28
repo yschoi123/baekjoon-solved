07ce7e0d883db55c34fa941d4c6906cd2646c975
YUNSEO CHOI <73926427+yschoi123@users.noreply.github.com>
2025-05-28 16:48:54 +0900

    [Silver III] Title: N과 M (5), Time: 1652 ms, Memory: 56540 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,48 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int[] array;
+    public static boolean[] check;
+    public static int[] res;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(array);
+
+        check = new boolean[n];
+        res = new int[m];
+
+        sequence(n, m, 0);
+    }
+
+    public static void sequence(int n, int m, int size) {
+       if(size == m) {
+           for(int j=0; j<m; j++) {
+               System.out.print(res[j]+" ");
+           }
+           System.out.println();
+           return;
+       }
+
+       for(int i=0; i<n; i++) {
+           if(!check[i]) {
+               res[size] = array[i];
+               check[i] = true;
+               sequence(n, m, size+1);
+               check[i] = false;
+           }
+       }
+    }
+}
